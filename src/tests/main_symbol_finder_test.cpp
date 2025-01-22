5770b0d7fa578462135ca1315fb58a2781ce01d4
Davit Kalantaryan <davit.kalantaryan@desy.de>
2025-01-22 11:26:54 +0100

    improvements on symbol finding api to check all ssl related symbols available

--- src/tests/main_symbol_finder_test.cpp
+++ src/tests/main_symbol_finder_test.cpp
@@ -20,79 +20,78 @@
 #include <cinternal/undisable_compiler_warnings.h>
 
 
-// #define SYMBOL_NAME_TO_FIND     "OPENSSL_init_ssl" // works (for OpenSSL_add_all_algorithms) if version is >= 0x10100000L
-// #define SYMBOL_NAME_TO_FIND     "SSL_library_init"
-// #define SYMBOL_NAME_TO_FIND     "OPENSSL_init_crypto" // works (for ERR_load_crypto_strings)
-// #define SYMBOL_NAME_TO_FIND     "BIO_new_mem_buf" // works
-// #define SYMBOL_NAME_TO_FIND     "PEM_read_bio_PrivateKey" // works
-// #define SYMBOL_NAME_TO_FIND     "EVP_PKEY_new"  // works
-// #define SYMBOL_NAME_TO_FIND     "EVP_PKEY_assign" // works (for EVP_PKEY_assign_RSA)
-// #define SYMBOL_NAME_TO_FIND     "EVP_MD_CTX_new"  // works
-// #define SYMBOL_NAME_TO_FIND     "EVP_PKEY_free"  // works
-// #define SYMBOL_NAME_TO_FIND     "BIO_free" // works
-// #define SYMBOL_NAME_TO_FIND     "EVP_DigestVerifyInit" // works
-// #define SYMBOL_NAME_TO_FIND     "EVP_DigestVerifyUpdate" // works
-// #define SYMBOL_NAME_TO_FIND     "EVP_DigestVerifyFinal" // works
-// #define SYMBOL_NAME_TO_FIND     "EVP_MD_CTX_free" // works
-// #define SYMBOL_NAME_TO_FIND     "EVP_PKEY_free" // works
-// #define SYMBOL_NAME_TO_FIND     "BIO_free" // works
-// #define SYMBOL_NAME_TO_FIND     "ERR_free_strings" // this is just a macro
-// #define SYMBOL_NAME_TO_FIND     "OPENSSL_init_ssl" // works
-// OPENSSL_cleanup
-// #define SYMBOL_NAME_TO_FIND     "OPENSSL_cleanup" // works
+typedef const char* const ConstCharPtr;
+
+static const ConstCharPtr  s_symbolsNames[] ={
+    "OPENSSL_init_ssl",
+    "OPENSSL_cleanup",
+    "BIO_new_mem_buf",
+    "PEM_read_bio_RSA_PUBKEY",
+    "BIO_free",
+    "EVP_PKEY_new",
+    "EVP_PKEY_assign",
+    "EVP_MD_CTX_new",
+    "EVP_PKEY_free",
+    "EVP_DigestVerifyInit",
+    "EVP_DigestUpdate",
+    "EVP_DigestVerifyFinal",
+    "EVP_sha256",
+    "EVP_MD_CTX_free"
+};
+
+
+static const size_t s_numberOfSymbols = sizeof(s_symbolsNames) / sizeof(const ConstCharPtr);
+
+static int ForceLoadSslLibraries(void);
+
 
 int main(int a_argc, char* a_argv[])
 {
+    size_t i;
     void* pFuncAddress;
-
-    pFuncAddress = SystemFindSymbolAddress(SYMBOL_NAME_TO_FIND);
-    qDebug()<<"1. pFuncAddress: "<<pFuncAddress;
 
     QCoreApplication aApp(a_argc, a_argv);
 
-    if (!QSslSocket::supportsSsl()) {
-        qDebug() << "OpenSSL is not available!";
-    } else {
-        qDebug() << "OpenSSL is available.";
-        // Optionally print available ciphers to verify initialization
-        qDebug() << "Supported ciphers:" ;
+    if(ForceLoadSslLibraries()){
+        return 1;
     }
 
-    pFuncAddress = SystemFindSymbolAddress(SYMBOL_NAME_TO_FIND);
-    qDebug()<<"2. pFuncAddress: "<<pFuncAddress;
+    for(i=0;i<s_numberOfSymbols;++i){
+        pFuncAddress= SystemFindSymbolAddress(s_symbolsNames[i]);
+        qDebug()<<s_symbolsNames[i] << " => "<<pFuncAddress;
+    }
 
-    QNetworkAccessManager manager;
-
-    pFuncAddress = SystemFindSymbolAddress(SYMBOL_NAME_TO_FIND);
-    qDebug()<<"3. pFuncAddress: "<<pFuncAddress;
-
-    // URL to fetch data from
-    QUrl url("https://jsonplaceholder.typicode.com/posts/1"); // Example URL
-    QNetworkRequest request(url);
-
-    // Send GET request
-    QNetworkReply *reply = manager.get(request);
-
-    // Connect the reply finished signal to handleNetworkReply
-    QObject::connect(reply, &QNetworkReply::finished, &aApp, [&reply]() {
-        if (reply->error() == QNetworkReply::NoError) {
-            // Print the data received from the URL
-            //qDebug() << "Response:" << reply->readAll();
-        } else {
-            // Print the error if any
-            qDebug() << "Error:" << reply->errorString();
-        }
-        reply->deleteLater(); // Clean up
-
-        void* pFuncAddress = SystemFindSymbolAddress(SYMBOL_NAME_TO_FIND);
-        qDebug()<<"4. pFuncAddress: "<<pFuncAddress;
-    });
-
-    qDebug()<<"5. pFuncAddress: "<<pFuncAddress;
-
-
-    aApp.exec();
-
+    //aApp.exec();
 
 	return 0;
+}
+
+
+static int ForceLoadSslLibraries(void)
+{
+    if (QSslSocket::supportsSsl()) {
+        qDebug() << "SSL Library Version:" << QSslSocket::sslLibraryVersionString();
+        qDebug() << "SSL Library Build Version:" << QSslSocket::sslLibraryBuildVersionString();
+
+        QNetworkAccessManager manager;
+        const QUrl url("https://google.com");
+        QNetworkRequest request(url);
+        QNetworkReply* const reply = manager.get(request);
+        QEventLoop loopUntilDone;
+        bool resultNotReady = true;
+        QObject::connect(reply, &QNetworkReply::finished, &manager, [&reply,&loopUntilDone,&resultNotReady]() {
+            resultNotReady = false;
+            loopUntilDone.quit();
+            reply->deleteLater(); // Clean up
+        });
+        if(resultNotReady){
+            loopUntilDone.exec();
+        }
+
+        return 0;
+    }
+
+
+
+    return 1;
 }
